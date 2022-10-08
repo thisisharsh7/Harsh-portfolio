79ec5d5ac7bb75db17b3ba5c28524391b4c42d7e
Harsh Kumar <9u.harsh@gmail.com>
2022-10-08 12:40:59 +0530

    project design on go

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,14 @@
 import Image from "next/image";
+import twind from "../public/Images/tailwind.svg";
+import saas from "../public/Images/saas.svg";
+import jscript from "../public/Images/javascript.png";
+import react from "../public/Images/react.svg";
+import nextjs from "../public/Images/nextjs.png";
+import git from "../public/Images/git.png";
+import clang from "../public/Images/clangu.png";
+import cplus from "../public/Images/cplus.png";
+import redux from "../public/Images/redux.svg";
+import firebase from "../public/Images/firebase.png";
 
 export default function about() {
   return (
@@ -22,18 +32,17 @@ export default function about() {
       </div>
       <div className="flex flex-col items-center gap-10">
         <h1 className="text-3xl relative font-semibold before:content-[''] before:w-full before:h-0.5 before:bottom-0 before:bg-black before:absolute">Skills</h1>
-        <ul className="flex flex-wrap gap-10 justify-center skill-list items-end">
-          <li><img src="https://tailwindcss.com/_next/static/media/tailwindcss-mark.79614a5f61617ba49a0891494521226b.svg" className="w-20"/>tailwindcss</li>
-          <li><img src="https://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg" className="w-20"/>Saas/Scss</li>
-          <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png" className="w-20"/>Javascript</li>
-          <li><img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K" className="w-20"/>React</li>
-          <li><img src="https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg" className="w-20"/>Redux</li>
-          <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Nextjs-logo.svg/800px-Nextjs-logo.svg.png" className="w-20"/>Next Js</li>
-          <li><img src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png" className="w-20"/>Git</li>
-          <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6ET6-zqVqg1I45mbg_PvlEd7JvZoXwspvjA&usqp=CAU" className="w-20"/>Firebase</li>
-          <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1UvDYFw7P_b96NMdT2uJJ7gskCz-1wjH9Pg&usqp=CAU" className="w-20"/>C</li>
-          <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxVrrgehMqY0gRr3ixmyc0-gZRPYAdh6lSNQ&usqp=CAU" className="w-20"/>C++</li>
-          <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXj2A2a5tQL05ZkHcEpl7NqsYSdhjnoPltgQ&usqp=CAU" className="w-20"/>Teaching</li>
+        <ul className="flex flex-wrap gap-10 justify-center skill-list items-center">
+          <li><Image src={twind} alt="twind" width={70} height={75}/><span className="bg-gray-200 py-1 px-2 rounded-md">tailwindcss</span></li>
+          <li><Image src={saas} alt="saas" width={70} height={75}/><span className="bg-gray-200 py-1 px-2 rounded-md">Saas/Scss</span></li>
+          <li><Image src={jscript} alt="jscript" width={70} height={75}/><span className="bg-gray-200 py-1 px-2 rounded-md">Javascript</span></li>
+          <li><Image src={react} alt="react" width={70} height={75}/><span className="bg-gray-200 py-1 px-2 rounded-md">React</span></li>
+          <li><Image src={nextjs} alt="nextjs" width={70} height={75}/><span className="bg-gray-200 py-1 px-2 rounded-md">Next Js</span></li>
+          <li><Image src={git} alt="git" width={70} height={75}/><span className="bg-gray-200 py-1 px-2 rounded-md">Git</span></li>
+          <li><Image src={clang} alt="clang" width={70} height={75}/><span className="bg-gray-200 py-1 px-2 rounded-md">C</span></li>
+          <li><Image src={cplus} alt="cplus" width={70} height={75}/><span className="bg-gray-200 py-1 px-2 rounded-md">C++</span></li>
+          <li><Image src={redux} alt="redux" width={70} height={75}/><span className="bg-gray-200 py-1 px-2 rounded-md">Redux</span></li>
+          <li><Image src={firebase} alt="firebase" width={70} height={75}/><span className="bg-gray-200 py-1 px-2 rounded-md">Firebase</span></li>
         </ul>
       </div>
     </section>
