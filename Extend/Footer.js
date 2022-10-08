48d2cc005cf2ce8a9856230cfcaefd012b347b9f
Harsh Kumar <9u.harsh@gmail.com>
2022-10-08 15:45:52 +0530

    porfolio on go

--- Extend/Footer.js
+++ Extend/Footer.js
@@ -1,26 +1,31 @@
+import Image from "next/image";
+import linkedInIc from "../public/Images/linkedInIc.svg";
+import gitHubIc from "../public/Images/gitIc.svg";
+import mailIc from "../public/Images/mailIc.svg";
+import twittIc from "../public/Images/twittIc.svg";
+import instaIc from "../public/Images/instaIc.svg";
+
 
 export default function Footer() {
   return (
-    <footer className="flex items-center flex-col gap-10">
-        <div className="flex  items-start w-full justify-between max-w-screen-md ">
+    <footer className="flex items-center flex-col gap-10 ">
+        <div className="flex  items-start w-full justify-between max-w-screen-md border-t-2 pt-5 ">
             <div className="flex flex-col gap-3">
-                <h1>Harsh Kumar</h1>
-                <p className="max-w-md">A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product.</p>
+                <h1 className="font-bold text-2xl">Harsh Kumar</h1>
+                <p className="max-w-md">The web is a place that I explore every day as a Web Developer.</p>
             </div>
             <div className="flex flex-col gap-3">
-                <h1>Profiles</h1>
+                <h1 className="text-xl text-gray-700 font-bold">Profiles</h1>
                 <ul className="flex gap-5">
-                    <li>linkedIn</li>
-                    <li>GitHub</li>
-                    <li>Email</li>
-                    <li>Twitter</li>
-                    <li>Dev</li>
-                    <li>Instagram</li>
+                    <li className="cursor-pointer"><Image src={gitHubIc} alt="GitHub" width={30} height={30}/></li>
+                    <li className="cursor-pointer"><Image src={linkedInIc} alt="LinkedIn" width={30} height={30}/></li>
+                    <li className="cursor-pointer"><Image src={mailIc} alt="Email" width={30} height={30}/></li>
+                    <li className="cursor-pointer"><Image src={twittIc} alt="Twitter" width={30} height={30}/></li>
+                    <li className="cursor-pointer"><Image src={instaIc} alt="Instagram" width={30} height={30}/></li>
                 </ul>
             </div>
         </div>
-        <p className="text-center">
-          Copyright © 2022. Made by <span>Harsh Kumar</span>
+        <p className="text-center">Developed by <span> Harsh Kumar</span> with <span>💖</span>
           </p>
     </footer>
   )
--- pages/contanct.js
+++ pages/contanct.js
@@ -2,7 +2,18 @@ export default function contanct(){
     return (
         <section className="w-full flex flex-col gap-12 max-w-screen-md pt-2 items-center ">
           <h1 className="text-3xl relative font-semibold before:content-[''] before:w-full before:h-0.5 before:bottom-0 before:bg-black before:absolute">Contanct</h1>
-          <div></div>
+          <div>
+            <form className=" p-2 flex flex-col gap-3">
+            <div className="flex gap-5 ">
+              <input type="text" className="p-1" placeholder="Name"/>
+      
+              <input type="email" className="p-1" placeholder="Email" />
+            </div>
+              <input type="text" className="w-full p-1" placeholder="Subject" />
+              <textarea  className="w-full h-40 p-1" placeholder="Message" />
+              <input type="submit" value="Send" className="bg-white self-start p-1" />
+            </form>
+          </div>
         </section>
       );
 }
