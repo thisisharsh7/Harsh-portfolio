75e559cc834ec3308dea0165a72c15d504aab92f
Harsh Kumar <9u.harsh@gmail.com>
2022-10-09 22:53:56 +0530

    portfolio on go

--- Extend/Contact.js
+++ Extend/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
   }, []);
   return (
     <section className="w-full flex flex-col gap-12 max-w-screen-md">
-      <div id="contanct" className="h-5"></div>
+      <div id="contact" className="h-5"></div>
       <h1 className="text-3xl relative font-semibold before:content-[''] before:w-full before:h-0.5 before:bottom-0 before:bg-black before:absolute">
         Contact Me
       </h1>
