5881da0e2b539c71e59de28723f544fc673beb3e
Harsh Kumar <9u.harsh@gmail.com>
2022-10-10 20:30:04 +0530

    portfolio link update

--- Extend/Header.js
+++ Extend/Header.js
@@ -5,13 +5,6 @@ import Link from "next/link";
 export default function Header() {
   const [Box, showBox] = useState(false);
   const route = useRouter();
-  function myFunction(link) {
-    showBox(false);
-
-    setTimeout(() => {
-      route.push(link);
-    }, 400);
-  }
   return (
     <header className=" sm:px-8 sm:py-9 pt-8 pb-4 px-4 flex max-w-[800px] mx-auto   justify-center min-w-[190px] bg-gray-100 shadow-sm sticky top-0 z-20">
       <nav className="flex  items-center w-full justify-between max-w-screen-md  relative ">
@@ -34,7 +27,7 @@ export default function Header() {
           className={
             Box
               ? " transition-all duration-700 flex sm:flex-row flex-col gap-10 items-center sm:relative absolute top-0 right-11  left-7 sm:bg-transparent bg-gray-100 px-3 py-5 z-10 sm:shadow-none shadow-md shadow-gray-700"
-              : " transition-all duration-1000 flex sm:shadow-none shadow-sm sm:flex-row flex-col sm:gap-10 gap-0 items-center  bg-transparent sm:relative absolute right-11 left-7 -top-44 sm:z-10 -z-20 sm:opacity-100  "
+              : " transition-all duration-1000 flex sm:shadow-none shadow-sm sm:flex-row flex-col sm:gap-10 gap-0 items-center  bg-transparent sm:relative absolute sm:right-0 sm:left-0 right-11 left-7 sm:top-0 -top-44 sm:z-10 -z-20 sm:opacity-100  "
           }
         >
           <li>
@@ -43,56 +36,77 @@ export default function Header() {
                 onClick={() => {
                   showBox(false);
                 }}
-                className="cursor-pointer  transition-all duration-300 hover:text-black text-gray-400"
+                className={
+                  route.asPath == "/"
+                    ? "cursor-pointer  transition-all duration-300 hover:text-black text-black"
+                    : "cursor-pointer  transition-all duration-300 hover:text-black text-gray-400"
+                }
               >
                 Home
               </a>
             </Link>
           </li>
           <li>
-            <a
-              href="#about"
-              onClick={() => {
-                showBox(false);
-              }}
-              className="cursor-pointer  transition-all duration-300 hover:text-black text-gray-400"
-            >
-              About
-            </a>
+            <Link href="/#about">
+              <a
+                onClick={() => {
+                  showBox(false);
+                }}
+                className={
+                  route.asPath == "/#about"
+                    ? "cursor-pointer  transition-all duration-300 hover:text-black text-black"
+                    : "cursor-pointer  transition-all duration-300 hover:text-black text-gray-400"
+                }
+              >
+                About
+              </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="#project"
-              onClick={() => {
-                showBox(false);
-              }}
-              className="cursor-pointer  transition-all duration-300 hover:text-black text-gray-400"
-            >
-              Projects
-            </a>
+            <Link href="/#project">
+              <a
+                onClick={() => {
+                  showBox(false);
+                }}
+                className={
+                  route.asPath == "/#project"
+                    ? "cursor-pointer  transition-all duration-300 hover:text-black text-black"
+                    : "cursor-pointer  transition-all duration-300 hover:text-black text-gray-400"
+                }
+              >
+                Projects
+              </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="#contact"
-              onClick={() => {
-                showBox(false);
-              }}
-              className="cursor-pointer  transition-all duration-300 hover:text-black text-gray-400 "
-            >
-              Contact
-            </a>
+            <Link href="/#contact">
+              <a
+                onClick={() => {
+                  showBox(false);
+                }}
+                className={
+                  route.asPath == "/#contact"
+                    ? "cursor-pointer  transition-all duration-300 hover:text-black text-black"
+                    : "cursor-pointer  transition-all duration-300 hover:text-black text-gray-400"
+                }
+              >
+                Contact
+              </a>
+            </Link>
           </li>
           <li>
-            <a
-              href="https://drive.google.com/file/d/1xPieQ4A12qqdDKeHrYhTBcS6jvjgQYJn/view"
-              target="_blank"
-              onClick={() => {
-                showBox(false);
-              }}
-              className="border-2 border-gray-400 px-2 py-1 cursor-pointer text-gray-400  transition-all duration-500 hover:border-black hover:text-black"
-            >
-              Resume
-            </a>
+            <Link href="https://drive.google.com/file/d/1xPieQ4A12qqdDKeHrYhTBcS6jvjgQYJn/view">
+              <a
+                target="_blank"
+                onClick={() => {
+                  showBox(false);
+                  route.push("/");
+                }}
+                className="border-2 border-gray-400 px-2 py-1 cursor-pointer text-gray-400  transition-all duration-500 hover:border-black hover:text-black"
+              >
+                Resume
+              </a>
+            </Link>
           </li>
         </ul>
       </nav>
